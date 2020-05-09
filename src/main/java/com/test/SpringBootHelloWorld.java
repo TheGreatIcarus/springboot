466b18cdb731d887370cf85c98c485119bca80f7
soul2046 <577579265@qq.com>
2020-05-09 09:58:51 +0800

    Initial commit

---
+++ src/main/java/com/test/SpringBootHelloWorld.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Array;
+
+@SpringBootApplication
+public class SpringBootHelloWorld{
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(SpringBootHelloWorld.class,args);
+
+
+
+
+    }
+}
+
